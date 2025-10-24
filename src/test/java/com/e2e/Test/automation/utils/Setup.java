1e9b4f9b60b47b818d7500ddc8f0157d9f23e3a4
maatougbesma <maatoug.basma@gmail.com>
2025-10-24 16:06:01 +0200

    add setup and login page feature

--- src/test/java/com/e2e/Test/automation/utils/Setup.java
+++ src/test/java/com/e2e/Test/automation/utils/Setup.java
@@ -1,30 +1,48 @@
 package com.e2e.Test.automation.utils;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
 
 public class Setup {
-	
+
 	private static WebDriver driver;
-	
+
 	public void setWebDriver() {
 		String browser = System.getProperty("browser");
 		if(browser == null) {
 			browser = "chrome";
-					}
-	switch (browser) {
-	case "chrome" :
-		System.setProperty("webdriver.chrome.driver", "src/test/resource/drivers/windows/chromedriver.exe");
-	ChromeOptions chromeOptions = new ChromeOptions();
-	driver = new ChromeDriver();
-	driver.manage().window().maximize();
-	
-	
+		}
+		switch (browser) {
+		case "chrome" :
+			System.setProperty("webdriver.chrome.driver", "src/test/resource/drivers/windows/chromedriver.exe");
+			ChromeOptions chromeOptions = new ChromeOptions();
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			chromeOptions.addArguments("['start-maximized']");
+			break;
+		case"firefox":
+			System.setProperty("webdriver.gecko.driver", "src/test/resource/drivers/windows/geckodriver.exe");
+			FirefoxProfile profile = new FirefoxProfile();
+			FirefoxOptions firefoxOptions = new FirefoxOptions();
+			firefoxOptions.setCapability("platform", Platform.WIN10);
+			firefoxOptions.setProfile(profile);
+			driver = new FirefoxDriver();
+			driver.manage().window().maximize();
+			break;
+			default:
+				throw new IllegalArgumentException("Browser \"" + browser + "\" is not supported. ");
+			
+			
+
+		}
+
 	}
-		
-	}
-		
-	
+
+
 
 }
--- target/classes/featuresFile/LoginPage.feature
+++ target/classes/featuresFile/LoginPage.feature
@@ -8,4 +8,5 @@ En tant que utilisateur, je souhaite naviguer
     When Je saisis le username "standard_user"
     And Je saisis le password "secret_sauce"
     And Je clique sur le bouton login
+    And test test
     Then Je me redirige vers la page home "PRODUCTS"
